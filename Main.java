b980c30f839f4ac5df92d3452d9472b6588de95c
Laura7229 <laura.alvarez4m7@gmail.com>
2025-10-20 23:30:01 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        CuentaBancaria cb = new CuentaBancaria(100);
-        cb.retirar(50);
-        cb.depositar(50);
-        cb.mostrarSaldo();
-    }
-}
