83d90aa458ef7568e03db03a889bf3a6fdae3537
Laura7229 <laura.alvarez4m7@gmail.com>
2025-10-26 18:09:57 +0100

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,22 +1,21 @@
-public class Estudiante extends Persona{
+import java.io.Serializable;
+
+public class Estudiante extends Persona implements Serializable {
+    private static final long serialVersionUID = 1L;
     private int ID_estudiante;
 
     public Estudiante(){
         super();
         this.ID_estudiante=0;
     }
-    public Estudiante(int ID_estudiante, String NIF, String apellidos, Direccion direccion, String nombre) {
-        super(NIF, apellidos, direccion, nombre);
+    // Constructor actualizado para Persona
+    public Estudiante(int ID_estudiante, String NIF, String apellidos, Direccion direccion, String nombre, String pin) {
+        super(NIF, apellidos, direccion, nombre, pin);
         this.ID_estudiante = ID_estudiante;
     }
 
-    public int getID_estudiante() {
-        return ID_estudiante;
-    }
-
-    public void setID_estudiante(int ID_estudiante) {
-        this.ID_estudiante = ID_estudiante;
-    }
+    public int getID_estudiante() { return ID_estudiante; }
+    public void setID_estudiante(int ID_estudiante) { this.ID_estudiante = ID_estudiante; }
     
     @Override
     public String toString() {
